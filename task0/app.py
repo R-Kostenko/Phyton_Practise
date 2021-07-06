2bc5cbb60b901710ba8fb8f3cd6ceedf11d21bdb
Rostislav Kostenko <kostenkorostik13052003@gmail.com>
2021-07-06 15:40:20 +0300

    final code

--- task0/app.py
+++ task0/app.py
@@ -1,6 +1,11 @@
 import random
-spysok=[]
+value=[]
 for i in range(30):
     k=random.randint(-100,100)
-    spysok.append(k)
-print (spysok)
+    value.append(k)
+print (value)
+print("\nМаксимальне число:",max(value))
+print("Індекс максимального числа:",value.index(max(value)))
+value_change=[i for i in value if not i%2]
+value_change.sort(reverse=True) 
+print("Відсортований список:", value_change)
